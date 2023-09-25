48f7b79701896b1242f3f72d90c3c387bf89ceee
Sasidhar Gajjala <sasidharpvl@gmail.com>
2023-09-25 00:52:05 -0400

    adding files firsttime

---
+++ 10-variable.tf
@@ -0,0 +1,12 @@
+variable "rg_name" {
+  default = "supermahesh"
+}
+variable "rg_location" {
+  default = "eastus"
+}
+variable "vnet_cider" {
+  default = "10.30.0.0/16"
+}
+#variable "env" { 
+#   default "developement"
+#}
